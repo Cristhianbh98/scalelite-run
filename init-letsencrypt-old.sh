03787489d942d2bff06c7a8343e6ee38c7a0b512
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2024-02-05 19:37:47 -0500

    update: init-letsencrypt

--- init-letsencrypt-old.sh
+++ init-letsencrypt-old.sh
@@ -13,59 +13,22 @@ if [[ ! -f ./.env ]]; then
   exit 1
 fi
 
-# Local .env
-if [ -f .env ]; then
-    # Load Environment Variables
-    export $(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )
-fi
-
-if [[ -z "$LETSENCRYPT_EMAIL" ]]; then
-  echo "Settung up an email for letsencrypt certificates is strongly recommended."
-  exit 1
-fi
-
-usage() {
-  echo -e "Initializes letsencrypt certificates for Nginx proxy container\n"
-  echo -e "Usage: $0 [-z|-r|-h]\n"
-  echo "  -n|--non-interactive  Enable non interactive mode"
-  echo "  -r|--replace          Replace existing certificates without asking"
-  echo "  -h|--help             Show usage information"
-  exit 1
-}
-
-interactive=1
-replaceExisting=0
-
-while [[ $# -gt 0 ]]
-do
-    case "$1" in
-        -n|--non-interactive) interactive=0;shift;;
-        -r|--replace) replaceExisting=1;shift;;
-        -h|--help) usage;;
-        -*) echo "Unknown option: \"$1\"\n";usage;;
-        *) echo "Script does not accept arguments\n";usage;;
-    esac
-done
-
+URL_HOST=$(grep URL_HOST .env | cut -d '=' -f2)
 echo $URL_HOST
 
 domains=($URL_HOST)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="$LETSENCRYPT_EMAIL" # Adding a valid address is strongly recommended.
-staging=${LETSENCRYPT_STAGING:-0}
+email="$LETSENCRYPT_EMAIL" # Adding a valid address is strongly recommended
+staging=${LETSENCRYPT_STAGING:-0} # Set to 1 if you're testing your setup to avoid hitting request limits
 
-if [ -d "$data_path" ] && [ "$replaceExisting" -eq 0 ]; then
-    if [ "$interactive" -eq 0 ]; then
-      echo "Certificates already exist."
-      exit
-    fi
-
-    read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-    if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-      exit
-    fi
+if [ -d "$data_path" ]; then
+  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+    exit
+  fi
 fi
+
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
@@ -85,6 +48,7 @@ docker-compose run --rm --entrypoint "\
     -subj '/CN=localhost'" certbot
 echo
 
+
 echo "### Starting scalelite-proxy ..."
 docker-compose up --force-recreate -d scalelite-proxy
 echo
@@ -95,6 +59,7 @@ docker-compose run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
 echo
+
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
 #Join $domains to -d args
@@ -115,7 +80,6 @@ if [ $staging != "0" ]; then staging_arg="--staging"; fi
 docker-compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
-    $([ "$interactive" -ne 1 ] && echo '--non-interactive') \
     $email_arg \
     $domain_args \
     --rsa-key-size $rsa_key_size \
@@ -124,5 +88,5 @@ docker-compose run --rm --entrypoint "\
     --force-renewal" certbot
 echo
 
-echo "### Reloading scalelite-proxy..."
-docker-compose exec $([ "$interactive" -ne 1 ] && echo "-T") scalelite-proxy nginx -s reload
+echo "### Reloading scalelite-proxy ..."
+docker-compose exec scalelite-proxy nginx -s reload
